32d6636013d035477e990066e607fff67f27cf9d
zhangrui <bubu.ok@163.com>
2021-02-15 02:49:02 +0800

    Eureka Server 注册 Provider“

--- service-provider-test/src/main/java/cn/xiaoyh/TestProvider.java
+++ service-provider-test/src/main/java/cn/xiaoyh/TestProvider.java
@@ -2,8 +2,10 @@ package cn.xiaoyh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TestProvider {
 
     public static void main(String[] args) {
