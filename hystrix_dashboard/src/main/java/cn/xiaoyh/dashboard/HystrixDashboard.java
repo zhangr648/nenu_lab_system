f6368f0d97fd2496401e03d8afe4fb24e6c9529f
zhangrui <bubu.ok@163.com>
2021-02-17 03:08:19 +0800

    添加 Hystrix Dashboard

---
+++ hystrix_dashboard/src/main/java/cn/xiaoyh/dashboard/HystrixDashboard.java
@@ -0,0 +1,14 @@
+package cn.xiaoyh.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboard {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboard.class, args);
+    }
+}
--- service-provider-test/src/main/java/cn/xiaoyh/TestProvider.java
+++ service-provider-test/src/main/java/cn/xiaoyh/TestProvider.java
@@ -1,9 +1,12 @@
 package cn.xiaoyh;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +15,12 @@ public class TestProvider {
 
     public static void main(String[] args) {
         SpringApplication.run(TestProvider.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        bean.addUrlMappings("/actuator/hystrix.stream");
+        return bean;
     }
 }
