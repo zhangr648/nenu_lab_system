d3e70b1fae99c27ab9124a598b97c40f1f853dd4
zhangrui <bubu.ok@163.com>
2021-02-17 22:03:57 +0800

    mybatis 与 数据库

--- feign-consumer-test/src/main/java/cn/xiaoyh/controller/PeopleConsumerController.java
+++ feign-consumer-test/src/main/java/cn/xiaoyh/controller/PeopleConsumerController.java
@@ -7,14 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class PeopleConsumerController {
 
     @Autowired
     private PeopleClientService service;
 
-    @GetMapping("/consumer/people/{id}")
+    @GetMapping("/people/{id}")
     public People getPeople(@PathVariable("id") Long id) {
         return service.queryById(id);
+    }
+
+    @GetMapping("/people/list")
+    public List<People> getAllPeople() {
+        return service.queryAll();
     }
 }
--- service-provider-test-8002/src/main/java/cn/xiaoyh/controller/PeopleController.java
+++ service-provider-test-8002/src/main/java/cn/xiaoyh/controller/PeopleController.java
@@ -1,7 +1,8 @@
 package cn.xiaoyh.controller;
 
+import cn.xiaoyh.dao.PeopleDao;
 import cn.xiaoyh.pojo.People;
-import cn.xiaoyh.service.PeopleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +12,19 @@ import java.util.List;
 @RestController
 public class PeopleController {
 
-    private final PeopleService peopleService;
+    private final PeopleDao peopleDao;
 
-    public PeopleController(PeopleService peopleService) {
-        this.peopleService = peopleService;
+    public PeopleController(PeopleDao peopleDao) {
+        this.peopleDao = peopleDao;
     }
 
     @GetMapping("/people/{id}")
     public People getPeople(@PathVariable("id") Long id) {
-        return peopleService.queryById(id);
+        return peopleDao.queryById(id);
     }
 
     @GetMapping("/people/list")
     public List<People> queryAll() {
-        return peopleService.queryAll();
+        return peopleDao.queryAll();
     }
 }
