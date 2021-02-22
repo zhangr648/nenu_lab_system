6c5c3fac724c45577ba0fb15440d3efb16112046
zhangrui <bubu.ok@163.com>
2021-02-23 02:05:14 +0800

    'websocket微服务'

---
+++ websocket-server/src/main/java/cn/xiaoyh/service/WebSocketServer.java
@@ -0,0 +1,53 @@
+package cn.xiaoyh.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@ServerEndpoint("/test")
+@Component
+public class WebSocketServer {
+
+    /** 记录当前在线连接数 */
+    private static final AtomicInteger onlineCount = new AtomicInteger(0);
+
+    @OnOpen
+    public void onOpen(Session session) {
+        onlineCount.incrementAndGet(); // 在线数加1
+        log.info("有新连接加入：{}，当前在线人数为：{}", session.getId(), onlineCount.get());
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        onlineCount.decrementAndGet(); // 在线数减1
+        log.info("有一连接关闭：{}，当前在线人数为：{}", session.getId(), onlineCount.get());
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        log.info("服务端收到客户端[{}]的消息:{}", session.getId(), message);
+        this.sendMessage("Hello, " + message, session);
+    }
+
+    @OnError
+    public void onError(Throwable error) {
+        log.error("发生错误", error);
+        error.printStackTrace();
+    }
+
+    /**
+     * 服务端发送消息给客户端
+     */
+    private void sendMessage(String message, Session toSession) {
+        try {
+            log.info("服务端给客户端[{}]发送消息{}", toSession.getId(), message);
+            toSession.getBasicRemote().sendText(message);
+        } catch (Exception e) {
+            log.error("服务端发送消息给客户端失败", e);
+        }
+    }
+}
