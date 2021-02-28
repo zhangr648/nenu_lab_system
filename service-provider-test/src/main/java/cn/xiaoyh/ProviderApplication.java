fb11176c8f04ec11ecff3fd198ae05e7e9e8dad5
xiaoyunhao <xiaoyunhao@bytedance.com>
2021-02-28 23:19:16 +0800

    合并两个 service-provider

--- service-provider-test/src/main/java/cn/xiaoyh/ProviderApplication.java
+++ service-provider-test/src/main/java/cn/xiaoyh/ProviderApplication.java
@@ -5,16 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableCircuitBreaker
-public class TestProvider {
+public class ProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestProvider.class, args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 
     @Bean
