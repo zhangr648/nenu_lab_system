613553cc3961a72413c5862f7c3743e32c2e396c
xiaoyunhao <xiaoyunhao@bytedance.com>
2021-03-31 00:13:29 +0800

    移除MP

--- nenu-user/src/main/java/cn/xiaoyh/UserApplication.java
+++ nenu-user/src/main/java/cn/xiaoyh/UserApplication.java
@@ -1,13 +1,20 @@
 package cn.xiaoyh;
 
-import org.mybatis.spring.annotation.MapperScan;
+import cn.xiaoyh.pojo.Result;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@MapperScan("cn.xiaoyh.nenu.mapper")
+@RestController
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+
+    @GetMapping("/**")
+    public Result test() {
+        return Result.ok().message("user");
     }
 }
